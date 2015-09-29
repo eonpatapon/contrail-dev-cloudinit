c9b4fad24412655e36e84c243be2df1fc5af5a78
Jean-Philippe Braun <eon@patapon.info>
2015-09-29 17:44:57 +0200

    Reorder networks and remove NAT mode

--- libvirt-install.sh
+++ libvirt-install.sh
@@ -21,4 +21,4 @@ virsh -c  qemu:///system net-info default || virsh -c  qemu:///system net-create
 virsh -c  qemu:///system net-info overlay || virsh -c  qemu:///system net-create conf/libvirt-net-overlay.xml
 virsh -c  qemu:///system net-info adm || virsh -c  qemu:///system net-create conf/libvirt-net-adm.xml
 
-virsh -c  qemu:///system dominfo ${2} || virt-install --connect qemu:///system --import -n $2 -r 4096 -w network=default -w network=overlay,model=virtio -w network=adm,model=virtio --disk path=$DISK_PATH --disk path=$ISO_PATH,device=cdrom --noautoconsole
+virsh -c  qemu:///system dominfo ${2} || virt-install --connect qemu:///system --import -n $2 -r 4096 -w network=adm,model=virtio -w network=overlay,model=virtio -w network=default --disk path=$DISK_PATH --disk path=$ISO_PATH,device=cdrom --noautoconsole
